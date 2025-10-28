421f38a07591f0a7e46316120d5441ee8efa8b5b
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2025-10-29 00:21:33 +0300

    Update paramvecformer.py

--- paramvecformer.py
+++ paramvecformer.py
@@ -1,10 +1,8 @@
 import torch
 from torch import nn
 
-
-
      
-class GlobalMappingUnit(nn.Module):
+class ParamVecFormerBlock(nn.Module):
     
     def __init__(self, dim,heads,num_paramvec):
             
@@ -22,64 +20,31 @@ class GlobalMappingUnit(nn.Module):
         
         batch_size, seq_len, _ = x.size()
         pvecs = self.paramvecs.repeat(batch_size,1,1)
-        
-        x = self.norm(x)
+        residual = x
         x = torch.cat((x,pvecs),dim=-2)
-        
+        x = self.norm(x)
+       
         P,S = x,x
        
-
-        
         P = P.view(batch_size, seq_len + self.num_paramvec, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
         S = S.view(batch_size, seq_len + self.num_paramvec, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
         
-
        
         attention_scores = P @ S.transpose(-1, -2) / math.sqrt(self.head_dim)
-
-       
 
         
         attention_weights = torch.softmax(attention_scores, dim=-1)
 
         
-
-       
         context = attention_weights @ S
 
         
         context = context.transpose(1, 2).contiguous().view(batch_size, seq_len + self.num_paramvec, self.hidden_dim)
 
-        
-        return context [:,0:seq_len,:]        
-
-
-
-
-class ParamVecFormerBlock(nn.Module):
-    def __init__(self, d_model,heads,num_paramvec):
-        super().__init__()
-       
+        pvecs = pvecs + context [:,seq_len:,:]
          
-        
-        self.global_mapping = GlobalMappingUnit(d_model,heads,num_paramvec)
-        
-    
-        
-        
-        
-    def forward(self, x):
-                  
-        residual = x
-        
-        x = self.global_mapping(x)
-    
-        x = x + residual
-        
-       
-        
-        
-        return x
+        return context [:,0:seq_len,:]  + residual [:,0:seq_len,:]      
+
 
 
 
